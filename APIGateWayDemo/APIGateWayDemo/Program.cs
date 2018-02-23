b682b63391bb5ee31fc5608e27503a86fac6f000
unknown <liurx@1handcar.com>
2018-02-23 17:44:49 +0800

    搭建APIGATEWAY

---
+++ APIGateWayDemo/APIGateWayDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace APIGateWayDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            IWebHostBuilder builder = new WebHostBuilder();
+
+            return builder.ConfigureServices(service =>
+            {
+                service.AddSingleton(builder);
+
+            })
+             .ConfigureAppConfiguration(conbuilder =>
+             {
+                 conbuilder.AddJsonFile("configuration.json");
+             })
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+        }
+    }
+}
